72fff7b869e473f94a7ba0abffb8798c899408a5
CJMeek <up887397@myport.ac.uk>
2020-08-05 17:11:51 +0100

    grid component
    Signed-off-by: CJMeek <up887397@myport.ac.uk>

--- src/app/landing-page/dashboard-boxes/dashboard-boxes.component.ts
+++ src/app/landing-page/dashboard-boxes/dashboard-boxes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Box } from '../../models/Box';
 
 @Component({
   selector: 'app-dashboard-boxes',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-boxes.component.scss']
 })
 export class DashboardBoxesComponent implements OnInit {
-
+  boxes:Box[];
   constructor() { }
 
   ngOnInit() {
+    this.boxes = [
+      {
+        title: "View past results",
+        description: "Lorem ipsum",
+        imagesrc: "src/assets/icons/icon-144x144.png"
+      },
+      {
+        title: "View systems data",
+        description: "Lorem ipsum",
+        imagesrc: "src/assets/icons/icon-144x144.png"
+      },
+      {
+        title: "Configure system",
+        description: "Lorem ipsum",
+        imagesrc: "src/assets/icons/icon-144x144.png"
+      },
+      {
+        title: "Submit tests",
+        description: "Lorem ipsum",
+        imagesrc: "src/assets/icons/icon-144x144.png"
+      }
+    ]
   }
 
 }
