08ceeeb1e257622f49d693109a4f292d84ae20d2
Diana Slepikaite <dianaslepikaite@Dianas-MacBook-Pro.local>
2020-08-05 14:15:21 +0100

    added dishboard box element
    Signed-off-by: CJMeek <up887397@myport.ac.uk>

---
+++ src/app/landing-page/dashboard-box/dashboard-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-box',
+  templateUrl: './dashboard-box.component.html',
+  styleUrls: ['./dashboard-box.component.scss']
+})
+export class DashboardBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
