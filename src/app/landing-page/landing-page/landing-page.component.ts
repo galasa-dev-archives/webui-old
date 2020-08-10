8d33e3a1cbac7286bcdbc8591c14034ee2fde060
Diana Slepikaite <d.slepikaite@gmail.com>
2020-08-10 14:28:44 +0100

    changed colours, altered flow, change name

--- src/app/landing-page/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'app-landing-page',
 	templateUrl: './landing-page.component.html',
-	styleUrls: ['./landing-page.component.scss']
+	styleUrls: ['./landing-page.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class LandingPageComponent {
 }
