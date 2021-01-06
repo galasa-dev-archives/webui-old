a34d0a97c24ef712a83243b8d7c7c49434f64d73
Jade Carino <jade.carino@ibm.com>
2021-01-06 10:15:11 +0000

    Started logic for Run Overview and Run Detail components
    Signed-off-by: Jade Carino <jade.carino@ibm.com>

--- src/app/run-page/components/run-overview/run-overview.component.ts
+++ src/app/run-page/components/run-overview/run-overview.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { IdeaComponent } from '@carbon/icons-angular';
 import { RasRunIdGetRequest, Run, RunResults, TestStructure } from 'galasa-ras-api-ts-rxjs';
 import { RasApisService } from '../../../core/rasapis.service';
+import { RasRunGetRequest } from 'galasa-ras-api-ts-rxjs';
 
 @Component({
   selector: 'app-run-overview',
@@ -12,16 +13,43 @@ import { RasApisService } from '../../../core/rasapis.service';
 export class RunOverviewComponent implements OnInit {
 
   @Input() testStructure: TestStructure;
-  changeLog: string[] = [];
+
+  attributes = [];
 
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
-    
+
+    let result = this.testStructure.result;
+    let runName = this.testStructure.runName;
+    let testShortName = this.testStructure.testShortName;
+    let bundle = this.testStructure.bundle;
+    let testName = this.testStructure.testName;
+    let requestor = this.testStructure.requestor;
+    let queued = this.testStructure.queued;
+    let startTime = this.testStructure.startTime;
+    let endTime = this.testStructure.endTime;
+   
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log(this.testStructure.runName);
-  }
 
+    // Populate Array with JSON objects with key and value
+    for (var key in this.testStructure){
+      if (key != "status" && key != "methods"){
+
+        var newObject = {
+          "key" : key,
+          "value" : this.testStructure[key]
+        }
+
+        this.attributes.push(newObject);
+      }
+    }
+
+    // Debug message
+    for (var x of this.attributes){
+      console.log("Key: " + x.key + " Value: " + x.value);
+    }
+  }
 }
