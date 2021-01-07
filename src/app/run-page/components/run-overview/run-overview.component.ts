0d75c9af2292245d406feae9485ec0996eac339f
Jade Carino <65225359+jadecarino@users.noreply.github.com>
2021-01-07 14:14:07 +0000

    Test information displayed in columns on Run Overview page (#40)
    * Started logic for Run Overview and Run Detail components
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Logic for Run Overview and Run Detail components, Date Time component added
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Test information displayed in columns on Run Overview page
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Fix
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Changes from PR
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>

--- src/app/run-page/components/run-overview/run-overview.component.ts
+++ src/app/run-page/components/run-overview/run-overview.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IdeaComponent } from '@carbon/icons-angular';
+import { IdeaComponent, MovementModule } from '@carbon/icons-angular';
 import { RasRunIdGetRequest, Run, RunResults, TestMethod, TestStructure } from 'galasa-ras-api-ts-rxjs';
 import { RasApisService } from '../../../core/rasapis.service';
+import { RasRunGetRequest } from 'galasa-ras-api-ts-rxjs';
 
 @Component({
   selector: 'app-run-overview',
@@ -11,17 +12,48 @@ import { RasApisService } from '../../../core/rasapis.service';
 })
 export class RunOverviewComponent implements OnInit {
 
-  @Input() testStructure: TestStructure = {};
+   @Input() testStructure: TestStructure = {};
+  
+  result: string = "";
+  runName: string = "";
+  testShortName: string = "";
+  bundle: string = "";
+  testName : string = "";
+  requestor: string = "";
+
+  queued : string = "";
+  startTime : string = "";
+  endTime : string = "";
+  startToEndTime : string = "";
+
   testMethods : TestMethod[] = [];
 
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
-
   }
 
   ngOnChanges(changes: SimpleChanges) {
+
+    this.result = this.testStructure.result;
+    this.runName = this.testStructure.runName;
+    this.testShortName = this.testStructure.testShortName;
+    this.bundle = this.testStructure.bundle;
+    this.testName = this.testStructure.testName;
+    this.requestor = this.testStructure.requestor;
+    this.queued = this.formatDate(this.testStructure.queued);
+    this.startTime = this.formatDate(this.testStructure.startTime);
+    this.endTime = this.formatDate(this.testStructure.endTime);
     this.testMethods = this.testStructure.methods;
+  }
+
+  formatDate(attribute : string){
+    // Translate into local browser time
+    var [date, time] = attribute.split('T');
+    time = time.slice(0, time.indexOf('Z'));
+    var dateTime = date + " " + time;
+    return dateTime;
+
   }
 
 }
