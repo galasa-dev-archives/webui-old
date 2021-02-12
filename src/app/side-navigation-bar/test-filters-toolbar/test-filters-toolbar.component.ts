8d2c5cbf15eae5ea7c1e45a4d5bd1888be57e08f
Jade Carino <65225359+jadecarino@users.noreply.github.com>
2021-02-12 10:25:52 +0000

    Side Navigation bar added with expandable toolbars, Breadcrumb finished, and further styling (#52)
    * Started logic for Run Overview and Run Detail components
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Logic for Run Overview and Run Detail components, Date Time component added
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Test information displayed in columns on Run Overview page
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Fix
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Changes from PR
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Logic added to translate date/time information to local browser time, and calculate and format the duration of the test
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Test result icons added, Run Overview component refactored
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Fix
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Code changes complete
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Commit
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Date time translation change
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Fix for date formatting to locale string
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Styling started on all existing pages
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Styling started for Tabs and Table components, Footer with Toggle added, Breadcrumb Header added
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Breadcrumb now one component, Icons and formatted Date Time added to Table components, some spacing and layout work started
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Run Overview layout responsive to screen sizes, Breadcrumbs static, Test Result icon resized, Results Table header styled
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Side Nav bar with expandable toolbars created, Breadcrumb finished, further styling changes
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Breadcrumb fix
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Breadcrumb fix 2
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>

---
+++ src/app/side-navigation-bar/test-filters-toolbar/test-filters-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-filters-toolbar',
+  templateUrl: './test-filters-toolbar.component.html',
+  styleUrls: ['./test-filters-toolbar.component.scss']
+})
+export class TestFiltersToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
